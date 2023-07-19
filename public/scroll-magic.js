3c6a1820361d20847201788ccecab51963ab5723
Eko Sutrisno <ekosutrisno801@gmail.com>
2023-07-19 17:54:55 +0700

    ui: remove unused component and add some animation interactivity

--- public/scroll-magic.js
+++ public/scroll-magic.js
@@ -2,44 +2,66 @@
 var controller = new ScrollMagic.Controller();
 
 // Beranda
-new ScrollMagic.Scene({
+const sceneBeranda = new ScrollMagic.Scene({
     triggerElement: '#trigger-beranda',
     duration: 400
 })
-    .setTween(TweenMax.from('#target-beranda', 1, { opacity: 0, scale: 0.80 }))
-    .addTo(controller);
+
+const timelineBeranda = new TimelineMax();
+timelineBeranda
+    .from('#target-beranda', 1, { opacity: 0, scale: 0.80 })
+    .from('#target-beranda-feat', { y: '25%', ease: Linear.easeNone, opacity: 0 });
+sceneBeranda.setTween(timelineBeranda).addTo(controller);
 
 // Profile
-new ScrollMagic.Scene({
+const sceneProfile = new ScrollMagic.Scene({
     triggerElement: '#trigger-profile',
     duration: 400
-})
-    .setTween(TweenMax.from('#target-profile', { y: '50%', x: '5%', ease: Linear.easeNone }))
-    .addTo(controller);
+});
+
+const timelineProfile = new TimelineMax();
+timelineProfile
+    .from('#target-profile', { y: '50%', x: '5%', ease: Linear.easeNone })
+    .from('#target-profile-feat', { y: '25%', ease: Linear.easeNone, opacity: 0 });
+sceneProfile.setTween(timelineProfile).addTo(controller);
 
 // Bacaanku
-new ScrollMagic.Scene({
+const sceneBacaanku = new ScrollMagic.Scene({
     triggerElement: '#trigger-bacaanku',
     duration: 400
-})
-    .setTween(gsap.from('#target-bacaanku', 1, { opacity: 0 }))
-    .addTo(controller);
+});
+
+const timelineBacaanku = new TimelineMax();
+timelineBacaanku
+    .from('#target-bacaanku', 1, { opacity: 0 })
+    .from('#target-bacaanku-feat', { y: '25%', ease: Linear.easeNone, opacity: 0 });
+sceneBacaanku.setTween(timelineBacaanku).addTo(controller);
 
 // Surat
-new ScrollMagic.Scene({
+const sceneSurat = new ScrollMagic.Scene({
     triggerElement: '#trigger-surat',
     duration: 400
-})
-    .setTween(TweenMax.from('#target-surat', 1, { opacity: 0, scale: 0.5 }))
-    .addTo(controller);
+});
+
+const timelineSurat = new TimelineMax();
+timelineSurat
+    .from('#target-surat', 1, { opacity: 0, scale: 0.5 })
+    .from('#target-surat-feat', { y: '25%', ease: Linear.easeNone, opacity: 0 });
+
+sceneSurat.setTween(timelineSurat).addTo(controller);
 
 // Kelas
-new ScrollMagic.Scene({
+const sceneKelas = new ScrollMagic.Scene({
     triggerElement: '#trigger-kelas',
     duration: 400
-})
-    .setTween(TweenMax.from('#target-kelas', { y: '25%', x: '-5%', ease: Linear.easeNone }))
-    .addTo(controller);
+});
+
+const timelineKelas = new TimelineMax();
+timelineKelas
+    .from('#target-kelas', { y: '25%', x: '-5%', ease: Linear.easeNone })
+    .from('#target-kelas-feat', { y: '25%', ease: Linear.easeNone, opacity: 0 });
+
+sceneKelas.setTween(timelineKelas).addTo(controller);
 
 // IMG CTA LIGHT
 const sceneImgLight = new ScrollMagic.Scene({
@@ -54,12 +76,9 @@ timelineImgLight
 sceneImgLight.setTween(timelineImgLight).addTo(controller);
 
 // IMG CTA DARK
-const sceneImgDark = new ScrollMagic.Scene({
+new ScrollMagic.Scene({
     triggerElement: '#cta-img-trigger',
     duration: 500
-});
-
-var timelineImgDark = new TimelineMax();
-timelineImgDark
-    .from('#cta-img-dark', 1, { y: '20%', x: '25%', ease: Linear.easeNone })
-sceneImgDark.setTween(timelineImgDark).addTo(controller);
+})
+    .setTween(TweenMax.from('#cta-img-dark', 1, { y: '20%', x: '25%', ease: Linear.easeNone }))
+    .addTo(controller);
