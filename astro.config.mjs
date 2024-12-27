487ac799c51865d5c325c5d302a0761e72bcb79b
Eko Sutrisno <ekosutrisno801@gmail.com>
2024-12-27 10:09:51 +0700

    seo: add robot.txt generator and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
+import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
+  site: 'https://kelaspondok.com',
   integrations: [tailwind(), partytown(
     {
       config: {
         forward: ["dataLayer.push"],
       },
     }
-  )],
+  ), robotsTxt(), sitemap()],
   server: {
     port: 3000
   }
