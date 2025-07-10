9c26d32c452c04060d00b8775a335ad4b6f4f40b
Eko Sutrisno <ekosutrisno801@gmail.com>
2025-07-10 10:37:38 +0700

    feat: add pricing plan information

--- src/types/common.types.ts
+++ src/types/common.types.ts
@@ -3,3 +3,13 @@ export interface Feature{
     title: string;
     description: string;
 }
+
+export interface PricingTier {
+  name: string;
+  id: string;
+  href: string;
+  priceMonthly: string;
+  description: string;
+  features: string[];
+  featured: boolean;
+}
